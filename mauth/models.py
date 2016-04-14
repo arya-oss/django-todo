a7e0635bf684b50b41909c075071a38ff84208eb
Rajmani Arya <rajmani1995@gmail.com>
2016-04-14 12:17:09 +0530

    authentication features completed except forgot password and deactivate

--- mauth/models.py
+++ mauth/models.py
@@ -1,5 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+# Authorization models
 
-# Create your models here.
+def upload_profile(instance, filename):
+    return 'profile/%s.jpg'%instance.user.username
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    phone = models.CharField(max_length=11, blank=True)
+    birthdate = models.DateField(null=True)
+    gender = models.CharField(max_length=1)
+    picture = models.ImageField(upload_to=upload_profile, blank=True)
+    ipAddress = models.CharField(max_length=20, blank=True)
+    def __unicode__(self):
+        return self.user.first_name
