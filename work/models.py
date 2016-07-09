c70a66626654a790806da2ce0435078acd01bbfb
Rajmani Arya <rajmani1995@gmail.com>
2016-07-09 11:41:53 +0000

    with some errors

--- work/models.py
+++ work/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 class todo(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    todo_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=250, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,9 +15,19 @@ class todo(models.Model):
     is_finished = models.BooleanField(default=False)
     priority = models.CharField(max_length=1)
     def __unicode__(self):
-        return self.user.username+" "+self.title
+        return self.title
 
 class tags(models.Model):
-    todo = models.ForeignKey(todo, on_delete=models.CASCADE)
+    tags_id = models.AutoField(primary_key=True)
     tagname = models.CharField(max_length=20)
-    description = models.CharField(max_length=100, blank=True, null=True);
+    description = models.CharField(max_length=100, blank=True, null=True)
+    def __unicode__(self):
+        return self.tagname
+
+class TodoTag(models.Model):
+	todo = models.ForeignKey(todo, on_delete=models.CASCADE)
+	tag = models.ForeignKey(tags, on_delete=models.CASCADE)
+
+class TodoUser(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	todo = models.ForeignKey(todo, on_delete=models.CASCADE)
